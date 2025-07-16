171cf27a7917c1ce0944653e237013707e738879
avinasha6 <avinash9999pabdey@gmail.com>
2025-07-16 12:09:30 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Header />
+        <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        &; 2023 My Website. All rights reserved. 
+    </div>
+  )
+}
+
+export default Footer
